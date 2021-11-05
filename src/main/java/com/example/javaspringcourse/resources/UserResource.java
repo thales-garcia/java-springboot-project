2e1d0157ed76536983d2b9b45c20b683720ad422
Thales Garcia <thalesgsoares@gmail.com>
2021-11-05 19:32:30 -0300

    Service layer, component registration

--- src/main/java/com/example/javaspringcourse/resources/UserResource.java
+++ src/main/java/com/example/javaspringcourse/resources/UserResource.java
@@ -1,19 +1,28 @@
 package com.example.javaspringcourse.resources;
 
 import com.example.javaspringcourse.entities.User;
+import com.example.javaspringcourse.repositories.UserRepository;
+import com.example.javaspringcourse.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Thales", "thales@gmail.com", "999999999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
     }
 
 
---
+++ src/main/java/com/example/javaspringcourse/services/UserService.java
@@ -0,0 +1,21 @@
+package com.example.javaspringcourse.services;
+
+import com.example.javaspringcourse.entities.User;
+import com.example.javaspringcourse.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+}
