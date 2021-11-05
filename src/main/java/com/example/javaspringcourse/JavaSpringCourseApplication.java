d758dd91d74a91ae4f1b02b10290cc65991314aa
Thales Garcia <thalesgsoares@gmail.com>
2021-11-05 16:34:49 -0300

    Project created

---
+++ src/main/java/com/example/javaspringcourse/JavaSpringCourseApplication.java
@@ -0,0 +1,14 @@
+package com.example.javaspringcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringCourseApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JavaSpringCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaspringcourse/JavaSpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
